e1d3f034da80420cc0cbb59aaf7d45f7bfd6c420
mnh4140 <71053769+mnh4140@users.noreply.github.com>
2023-07-28 16:45:41 +0900

    Update Rocky_Linux_Init.sh

--- Rocky_Linux_Init.sh
+++ Rocky_Linux_Init.sh
@@ -1,7 +1,7 @@
 date=$(date '+%Y%m%d_%H%M')
 logfilename="$date"
-logdate=$(date '+[%Y-%m-%d %H:%M:%S]')
-
+#logdate=$(date '+[%Y-%m-%d %H:%M:%S]')
+ldate=`date '+[%Y-%m-%d %H:%M:%S]'`
 logfile=$(pwd)/$logfilename.log
 
 
@@ -23,6 +23,13 @@ function test()
 	linebreak
 	linebreak
 }
+
+function logtime()
+{
+        ldate=`date '+[%Y-%m-%d %H:%M:%S]'`
+        #echo -e "$ldate"
+}
+
 #################################
 
 ### STEP 1 #########################
@@ -31,19 +38,16 @@ function test()
 
 echo '### STEP 1 UTC to KST Converter' >> $(pwd)/$logfilename.log
 
-test
-echo 'test'
-test
-test
-
 echo '' >> $(pwd)/$logfilename.log
 
 
 echo '!) EXECUTE COMMAND LOG' >> $(pwd)/$logfilename.log
-echo "$logdate          ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime" >> $(pwd)/$logfilename.log
+logtime
+echo -e "$ldate\t\tln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime" >> $(pwd)/$logfilename.log
 #echo 'ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime' >> $(pwd)/$logfilename.log
 
 echo '' >> $(pwd)/$logfilename.log
+
 echo '!) COMMAND RESULT' >> $(pwd)/$logfilename.log
 #timedatectl | grep Time >> $(pwd)/$logfilename.log
 timedatectl | awk -F":" '/Time zone/ {print $0}' | sed 's/^ *//g' >> $(pwd)/$logfilename.log
@@ -53,14 +57,18 @@ echo '' >> $(pwd)/$logfilename.log
 echo '--------------------------------' >> $(pwd)/$logfilename.log
 echo '' >> $(pwd)/$logfilename.log
 
+#sleep 1
+
 ### STEP 2 #########################
 echo '### STEP 2 History Time Logging' >> $(pwd)/$logfilename.log
 
 echo '' >> $(pwd)/$logfilename.log
 
 echo '!) EXECUTE COMMAND LOG' >> $(pwd)/$logfilename.log
-echo "$logdate          echo "HISTTIMEFORMAT=\"[%Y-%m-%d_%H:%M:%S]  \"" >> /etc/profile" >> $(pwd)/$logfilename.log
-echo "$logdate          source /etc/profile" >> /etc/profile"" >> $(pwd)/$logfilename.log
+logtime
+echo -e "$ldate\t\techo "HISTTIMEFORMAT=\"[%Y-%m-%d_%H:%M:%S]  \"" >> /etc/profile" >> $(pwd)/$logfilename.log
+logtime
+echo -e "$ldate\t\tsource /etc/profile" >> /etc/profile"" >> $(pwd)/$logfilename.log
 
 echo '' >> $(pwd)/$logfilename.log
 
@@ -74,7 +82,7 @@ echo '' >> $(pwd)/$logfilename.log
 
 
 
-
+#sleep 1
 #####################################
 ### STEP 3 #########################
 
@@ -82,22 +90,27 @@ echo '### STEP 3 SSH Password Login' >> $logfile
 linebreak
 
 echo '!) EXECUTE COMMAND LOG' >> $logfile
-echo "sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config" >> $logfile
-echo "sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config" >> $logfile
-echo "sed -i '/PasswordAuthentication no/ s/^/#/' /etc/ssh/sshd_config" >> $logfile
-echo "sed -i '/GSSAPIAuthentication yes/ s/^/#/' /etc/ssh/sshd_config" >> $logfile
-echo "systemctl restart sshd" >> $logfile
+logtime
+echo -e "$ldate\t\tsed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config" >> $logfile
+logtime
+echo -e "$ldate\t\tsed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config" >> $logfile
+logtime
+echo -e "$ldate\t\tsed -i '/PasswordAuthentication no/ s/^/#/' /etc/ssh/sshd_config" >> $logfile
+logtime
+echo -e "$ldate\t\tsed -i '/GSSAPIAuthentication yes/ s/^/#/' /etc/ssh/sshd_config" >> $logfile
+logtime
+echo -e "$ldate\t\tsystemctl restart sshd" >> $logfile
 
 echo '' >> $logfile
 
 echo '!) COMMAND RESULT' >> $logfile
-sed -n '/PermitRootLogin no/p' /etc/ssh/sshd_config >> $$logfile
+sed -n '/PermitRootLogin no/p' /etc/ssh/sshd_config >> $logfile
 sed -n '/PasswordAuthentication yes/p' /etc/ssh/sshd_config >> $logfile
 sed -n '/GSSAPIAuthentication yes/p' /etc/ssh/sshd_config >> $logfile
 systemctl status sshd | awk -F":" '/Active/ {print $0}' | sed 's/^ *//g' >> $logfile
 
 dash
-
+#sleep 1
 #####################################
 ### STEP 4 #########################
 
@@ -105,8 +118,10 @@ echo '### STEP 4 Password Change' >> $logfile
 linebreak
 
 echo '!) EXECUTE COMMAND LOG' >> $logfile
-echo "echo 'Sniper13@$' | passwd --stdin root" >> $logfile
-echo "echo 'Sniper13@$' | passwd --stdin rocky" >> $logfile
+logtime
+echo -e "$ldate\t\techo 'Sniper13@$' | passwd --stdin root" >> $logfile
+logtime
+echo -e "$ldate\t\techo 'Sniper13@$' | passwd --stdin rocky" >> $logfile
 
 echo '' >> $logfile
 
@@ -114,7 +129,7 @@ echo '!) COMMAND RESULT' >> $logfile
 echo 'No Result' >> $logfile
 
 dash
-
+#sleep 1
 #####################################
 ### STEP 5 #########################
 
@@ -122,7 +137,8 @@ echo '### STEP 5 FTP root Login' >> $logfile
 linebreak
 
 echo '!) EXECUTE COMMAND LOG' >> $logfile
-echo "echo "root" >> /etc/ftpusers" >> $logfile
+logtime
+echo -e "$ldate\t\techo "root" >> /etc/ftpusers" >> $logfile
 
 echo '' >> $logfile
 
@@ -130,7 +146,7 @@ echo '!) COMMAND RESULT' >> $logfile
 cat /etc/ftpusers >> $logfile
 
 dash
-
+#sleep 1
 ##################################
 
 
@@ -141,8 +157,10 @@ echo '### STEP 6 Selinux Disable' >> $logfile
 linebreak
 
 echo '!) EXECUTE COMMAND LOG' >> $logfile
-echo "setenforce 0" >> $logfile
-echo "sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config" >> $logfile
+logtime
+echo -e "$ldate\t\tsetenforce 0" >> $logfile
+logtime
+echo -e "$ldate\t\tsed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config" >> $logfile
 
 linebreak
 
@@ -152,25 +170,29 @@ linebreak
 echo '!!! you must be reboot !!!' >> $logfile
 
 dash
-
+#sleep 1
 #####################################
 ### STEP 7 #########################
 
 echo '### STEP 7 Firewalld Disable' >> $logfile
 linebreak
 
+
 echo '!) EXECUTE COMMAND LOG' >> $logfile
-echo "systemctl stop firewalld && systemctl disable firewalld" >> $logfile
+logtime
+echo -e "$ldate\t\tsystemctl stop firewalld && systemctl disable firewalld" >> $logfile
+
+
 
 linebreak
 
 echo '!) COMMAND RESULT' >> $logfile
-systemctl status firewalld >> $logfile
+systemctl status firewalld >> $logfile # 서비스 없으면 에러 뜸 if문으로 개선 해야될 듯 해
+
 linebreak
-echo '!!! you must be reboot !!!' >> $logfile
 
 dash
-
+#sleep 1
 #####################################
 ### STEP 8 #########################
 
@@ -178,13 +200,42 @@ echo '### STEP 8 Network Tool Install ' >> $logfile
 linebreak
 
 echo '!) EXECUTE COMMAND LOG' >> $logfile
-echo "dnf install iputils net-tools tcpdump -y" >> $logfile
+logtime
+echo -e "$ldate\t\tdnf install iputils net-tools tcpdump -y" >> $logfile
 
 linebreak
 
 echo '!) COMMAND RESULT' >> $logfile
-systemctl status firewalld >> $logfile
+dnf info iputils net-tools tcpdump | sed -n '/Name/p' >> $logfile
+
 linebreak
-echo '!!! you must be reboot !!!' >> $logfile
+
+dash
+#sleep 1
+#####################################
+### STEP 9 #########################
+
+echo '### STEP 9 NTP Client Configuration ' >> $logfile
+linebreak
+echo '!) EXECUTE COMMAND LOG' >> $logfile
+logtime
+echo -e "$ldate\t\tsystemctl stop chronyd" >> $logfile
+logtime
+echo -e "$ldate\t\tsed -i '/pool 2.rocky.pool.ntp.org iburst/ s/^/#/' /etc/chrony.conf" >> $logfile
+logtime
+echo -e "$ldate\t\tsed -i '/pool 2.rocky.pool.ntp.org iburst/a\server time.bora.net iburst' /etc/chrony.conf" >> $logfile
+logtime
+echo -e "$ldate\t\tsystemctl start chronyd" >> $logfile
+logtime
+echo -e "$ldate\t\tchronyc sources" >> $logfile
+
+linebreak
+
+echo '!) COMMAND RESULT' >> $logfile
+systemctl status chronyd | awk -F":" '/Active/ {print $0}' | sed 's/^ *//g' >> $logfile
+sed -n '/iburst/p' /etc/chrony.conf >> $logfile
+chronyc sources >> $logfile
+
+linebreak
 
 dash
